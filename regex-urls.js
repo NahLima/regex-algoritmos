75495c9bcc477f3061c96b0a55b2ce503c6074cb
NahLima <naiarafx@gmail.com>
2021-09-06 16:46:36 -0300

    converte apenas o que for válido

--- regex-urls.js
+++ regex-urls.js
@@ -1,16 +1,18 @@
 let urls = [ 'https://imagens-revista.vivadecora.com.br/uploads/2020/06/bolino-de-chuva-quality-refeições.jpg', 
-"https://imagens-revista.vivadecora.com.br/uploads/2021/08/Ideias-de-cores-para-sala-de-estar-pequena-decorada-com-tapete-listrado-e-almofadas-coloridas-Foto-Home-Interior-Ideas.jpg", "https://imagens-revista.vivadecora.com.br/uploads/2021/08/Cores-neutras-para-decoração-de-sala-pequena-de-apartamento-Foto-HomisHome.jpg"];
+"https://imagens-revista.vivadecora.com.br/uploads/2021/08/Ideias-de-cores-para-sala-de-estar-pequena-decorada-com-tapete-listrado-e-almofadas-coloridas-Foto-Home-Interior-Ideas.jpg", "https://imagens-revista.vivadecora.com.br/uploads/2021/08/Cores-neutras-para-decoração-de-sala-pequena-de-apartamento-Foto-HomisHome.jpg", 
+"https://imagens-revista.vivadecora.com.br/uploads/2021/08/Cores-para-sala-de-jantar-pequena-decorada-com-cadeiras-amarelas-para-mesa-de-vidro-redonda-Foto-Yamagata-Arquitetura.jpg"];
 
 const regex = /^[^ãáàâéêíîóôõúç]+$/i;
-
+//let valido = false
+//let urlsOk = []
 urls.forEach(pathImagem => {
     let matchUrl = pathImagem.split(/ +/).every(parte => regex.test(parte)); //  retorna true or false 
     //console.log(matchUrl)  
     //console.log(`${pathImagem} = ${matchUrl ? 'válido': 'inválido'}`);
 
-    // falta uma lógica para ativar essa parte de baixo
-    if ( urls == true){
-        novaUrl = urls.replace(/(\.jpg|\.png|\.jpeg)$/g,'.webp')
+    if ( matchUrl == true){
+        novaUrl = pathImagem.replace(/(\.jpg|\.png|\.jpeg)$/g,'.webp')
+        console.log(novaUrl)
     }
 });
 
