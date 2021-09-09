39c68698d2c609c3a43d8c4e020d72d72ac89b73
NahLima <naiarafx@gmail.com>
2021-09-09 15:34:07 -0300

    for of

--- regex-urls-forOf.js
+++ regex-urls-forOf.js
@@ -1,25 +1,37 @@
-let urls = ['https://imagens-revista.vivadecora.com.br/uploads/2020/06/bolino-de-chuva-quality-refeições.jpg',
+let fwdUri = ['https://imagens-revista.vivadecora.com.br/uploads/2020/06/bolino-de-chuva-quality-refeições.jpg',
     "https://imagens-revista.vivadecora.com.br/uploads/2021/08/Ideias-de-cores-para-sala-de-estar-pequena-decorada-com-tapete-listrado-e-almofadas-coloridas-Foto-Home-Interior-Ideas.jpg", "https://imagens-revista.vivadecora.com.br/uploads/2021/08/Cores-neutras-para-decoração-de-sala-pequena-de-apartamento-Foto-HomisHome.jpg",
     "https://imagens-revista.vivadecora.com.br/uploads/2021/08/Cores-para-sala-de-jantar-pequena-decorada-com-cadeiras-amarelas-para-mesa-de-vidro-redonda-Foto-Yamagata-Arquitetura.jpg",
     "https://imagens-revista.vivadecora.com.br/uploads/2021/08/Cores-para-sala-de-estar-pequena-decorada-com-sofá-cinza-e-quadro-grande-Foto-Carlos-Piratininga.jpg",
     "https://imagens-revista.vivadecora.com.br/uploads/2021/08/Cores-para-sala-pequena-decorada-com-estilo-industrial-Foto-Mandril-Arquitetura.jpg"];
 
 
+
+
 const regex = /^[^ãáàâéêíîóôõúç]+$/i;
-let allUrls = []
-urls.forEach(pathImagem => {
-    let matchUrl = pathImagem.split(/ +/).every(parte => regex.test(parte)); //  retorna true or false 
-    console.log(matchUrl)  
-    console.log(`${pathImagem} = ${matchUrl ? 'válido': 'inválido'}`);
+let newFwdUri = []
 
+for (let value of fwdUri){
+    let matchUrl = value.split(/ +/).every(parte => regex.test(parte)); //  retorna true or false 
+    
     if (matchUrl == true) {
-        novaUrl = pathImagem.replace(/(\.jpg|\.png|\.jpeg)$/g, '.webp')
-        allUrls.push(novaUrl)
-        //console.log(novaUrl)
-    } else {
-        allUrls.push(pathImagem)
-        //console.log(pathImagem);
+        fwdUri = value.replace(/(\.jpg|\.png|\.jpeg)$/g, '.webp')
+        newFwdUri.push(fwdUri)
+        //console.log(newFwdUri)
+    }else{
+        fwdUri = value
+        newFwdUri.push(fwdUri)
+        //console.log(newFwdUri)
     }
-})
+} 
 
-console.log(allUrls);
+console.log(newFwdUri)
+    
+
+
+
+
+// urls sem acento convertidas para webp + urls sem converter
+//console.log(urls);
+
+
+
